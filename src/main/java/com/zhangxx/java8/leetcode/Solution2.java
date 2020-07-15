45e4b2ccfb7e51e0c6686bba95c6803a52250d80
bukoudai <secret0me@qq.com>
2020-07-15 10:42:56 +0800

    63. 不同路径 II

--- src/main/java/com/zhangxx/java8/leetcode/Solution2.java
+++ src/main/java/com/zhangxx/java8/leetcode/Solution2.java
@@ -43,6 +43,78 @@ public class Solution2 {
     }
 
 
+    /**
+     * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+     * <p>
+     * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+     * <p>
+     * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+     * 网格中的障碍物和空位置分别用 1 和 0 来表示。
+     * <p>
+     * 说明：m 和 n 的值均不超过 100。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入:
+     * [
+     *   [0,0,0],
+     *   [0,1,0],
+     *   [0,0,0]
+     * ]
+     * 输出: 2
+     * 解释:
+     * 3x3 网格的正中间有一个障碍物。
+     * 从左上角到右下角一共有 2 条不同的路径：
+     * 1. 向右 -> 向右 -> 向下 -> 向下
+     * 2. 向下 -> 向下 -> 向右 -> 向右
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/unique-paths-ii
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param obstacleGrid
+     * @return
+     */
+    public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid.length == 0) {
+            return 0;
+        }
+
+        int n = obstacleGrid.length;
+        int m = obstacleGrid[0].length;
+        int[][] dp = new int[n][m];
+
+        if (obstacleGrid[n - 1][m - 1] == 0) {
+            dp[n - 1][m - 1] = 1;
+        } else {
+            dp[n - 1][m - 1] = 0;
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                //判断左和上是否有障碍物或边界
+                if (i == (n - 1) && j == (m - 1)) {
+                    continue;
+                }
+                //左             上
+                int z = 0;
+                if (j + 1 < m) {
+                    z = dp[i][j + 1];
+                }
+                int s = 0;
+                if (i + 1 < n) {
+                    s = dp[i + 1][j];
+                }
+                if (obstacleGrid[i][j] == 0) {
+                    dp[i][j] = s + z;
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
 }
 
 
--- src/test/java/LeetCodeTest2.java
+++ src/test/java/LeetCodeTest2.java
@@ -17,5 +17,27 @@ public class LeetCodeTest2 {
 
     }
 
+    @Test
+    public void uniquePathsWithObstacles() {
+        int[] a = {1};
+        int[] b = {0, 0, 0};
+        int[] c = {0, 0, 0};
+
+        /**
+         *   int[] a = { 18,4,-3};
+         *         int[] b = { 16,11,5};
+         *         int[] c = { -14,-24,6};
+         */
+        int[][] t = new int[1][1];
+        t[0] = a;
+//        t[1] = b;
+//        t[2] = c;
+
+
+        System.out.println(Solution2.uniquePathsWithObstacles(t));
+
+
+    }
+
 
 }
