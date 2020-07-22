3589b03bed9e470fa16501fc51a92866a279c784
bukoudai <secret0me@qq.com>
2020-07-22 17:16:49 +0800

    SVN 工具

---
+++ src/main/java/com/zhangxx/java8/svn/MyDefaultTableModel.java
@@ -0,0 +1,18 @@
+package com.zhangxx.java8.svn;
+
+import javax.swing.table.DefaultTableModel;
+
+public class MyDefaultTableModel extends DefaultTableModel {
+
+    public MyDefaultTableModel(String[] columns, int i) {
+        super(columns,i);
+    }
+
+
+    @Override
+    public boolean isCellEditable(int row, int column)
+    {
+        return false;
+    }
+}
+
