0e92deeb868c0513b94c1dcca7e632ea9fe192f2
bukoudai <secret0me@qq.com>
2020-07-23 16:40:18 +0800

    11. 盛最多水的容器

---
+++ src/test/java/LeetCodeTest5.java
@@ -0,0 +1,16 @@
+import com.zhangxx.java8.leetcode.Solution5;
+import org.junit.Test;
+
+public class LeetCodeTest5 {
+
+    @Test
+    public void sortedArrayToBST() {
+
+        int[] a = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+
+
+//        System.out.println( Solution5.maxArea(a));
+        System.out.println(Solution5.maxArea2(a));
+
+    }
+}
