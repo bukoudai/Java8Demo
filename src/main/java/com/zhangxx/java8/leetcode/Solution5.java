7499578ffba42f5f4a250c46a7a227d9fffc55a8
bukoudai <secret0me@qq.com>
2020-07-24 15:10:41 +0800

    15. 三数之和 16. 最接近的三数之和

--- src/main/java/com/zhangxx/java8/leetcode/Solution5.java
+++ src/main/java/com/zhangxx/java8/leetcode/Solution5.java
@@ -1,5 +1,7 @@
 package com.zhangxx.java8.leetcode;
 
+import java.util.*;
+
 public class Solution5 {
 
     /**
@@ -107,5 +109,227 @@ public class Solution5 {
         //N初始若为奇数，那么爱丽丝第一次选完之后N必为偶数，那么鲍勃只需一直选1就会稳赢。
         //综述，判断N是奇数还是偶数，即可得出最终结果！
         return N % 2 == 0;
+    }
+
+    /**
+     * 15. 三数之和
+     * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+     * <p>
+     * 注意：答案中不可以包含重复的三元组。
+     * <p>
+     * <p>
+     * <p>
+     * 示例：
+     * <p>
+     * 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+     * <p>
+     * 满足要求的三元组集合为：
+     * [
+     * [-1, 0, 1],
+     * [-1, -1, 2]
+     * ]
+     *
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> threeSum(int[] nums) {
+        //超时..
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            Set<Integer> map = new HashSet<>();
+            for (int j = 0; j < nums.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+                int complement = -nums[i] - nums[j];
+                if (map.contains(complement)) {
+                    List<Integer> list = new ArrayList<>();
+                    boolean d = false;
+                    list.add(nums[i]);
+                    list.add(complement);
+                    list.add(nums[j]);
+                    list.sort(Integer::compare);
+                    for (List<Integer> an : ans) {
+                        if (an.get(0).equals(list.get(0)) &&
+                                an.get(1).equals(list.get(1)) &&
+                                an.get(2).equals(list.get(2))) {
+                            d = true;
+                            break;
+                        }
+
+                    }
+                    if (!d) {
+                        ans.add(list);
+                    }
+
+
+                }
+                map.add(nums[j]);
+            }
+        }
+
+
+        return ans;
+    }
+
+    //-5 -3 -2 -1 0 1 2 3 4 5
+    public static List<List<Integer>> threeSum2(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums.length < 3) {
+            return ans;
+
+        }
+        Arrays.sort(nums);
+        for (int k = 0; k < nums.length - 2; k++) {
+            if ((k - 1 >= 0) && (nums[k] == nums[k - 1])) {
+                continue;
+            }
+            int left = k + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                if ((left - 1) > k && nums[left - 1] == nums[left]) {
+                    left++;
+                    continue;
+                }
+                if ((right + 1) < (nums.length - 1) && nums[right + 1] == nums[right]) {
+                    right--;
+                    continue;
+                }
+
+                int i = nums[k] + nums[right] + nums[left];
+
+                if (i < 0) {
+                    left++;
+                } else if (i > 0) {
+                    right--;
+                } else {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[k]);
+                    list.add(nums[left]);
+                    list.add(nums[right]);
+                    ans.add(list);
+                    left++;
+                    right--;
+                }
+
+
+            }
+
+        }
+
+        return ans;
+    }
+
+    /**
+     * 16. 最接近的三数之和
+     * 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+     * <p>
+     *  
+     * <p>
+     * 示例：
+     * <p>
+     * 输入：nums = [-1,2,1,-4], target = 1
+     * 输出：2
+     * 解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
+     *  
+     * <p>
+     * 提示：
+     * <p>
+     * 3 <= nums.length <= 10^3
+     * -10^3 <= nums[i] <= 10^3
+     * -10^4 <= target <= 10^4
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/3sum-closest
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int threeSumClosest(int[] nums, int target) {
+        int ans = 0;
+        if (nums.length == 3) {
+            return 0;
+
+        }
+        Arrays.sort(nums);
+        boolean frist = true;
+        for (int k = 0; k < nums.length - 2; k++) {
+            if ((k - 1 >= 0) && (nums[k] == nums[k - 1])) {
+                continue;
+            }
+            int left = k + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                if ((left - 1) > k && nums[left - 1] == nums[left]) {
+                    left++;
+                    continue;
+                }
+                if ((right + 1) < (nums.length - 1) && nums[right + 1] == nums[right]) {
+                    right--;
+                    continue;
+                }
+
+                int i = nums[k] + nums[right] + nums[left];
+
+                if (i < target) {
+                    left++;
+                } else if (i > target) {
+                    right--;
+                } else {
+
+                    left++;
+                    right--;
+                }
+                if (frist) {
+                    ans = i;
+                    frist = false;
+                } else if (Math.abs(target - i) <= Math.abs(target - ans)) {
+                    ans = i;
+                }
+
+            }
+
+        }
+
+        return ans;
+    }
+
+    /**
+     * 1. 两数之和
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     * <p>
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+     * <p>
+     *  
+     * <p>
+     * 示例:
+     * <p>
+     * 给定 nums = [2, 7, 11, 15], target = 9
+     * <p>
+     * 因为 nums[0] + nums[1] = 2 + 7 = 9
+     * 所以返回 [0, 1]
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/two-sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
--- src/test/java/LeetCodeTest5.java
+++ src/test/java/LeetCodeTest5.java
@@ -13,4 +13,34 @@ public class LeetCodeTest5 {
         System.out.println(Solution5.maxArea2(a));
 
     }
+
+    @Test
+    public void threeSum() {
+
+        int[] a = {-1, 0, 1, 2, -1, -4};
+        int[] b = {-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6};
+        int[] c = {-5, 0, -2, 3, -2, 1, 1, 3, 0, -5, 3, 3, 0, -1};
+
+
+        System.out.println(Solution5.threeSum(a));
+        System.out.println(Solution5.threeSum2(a));
+        System.out.println(Solution5.threeSum(b));
+        System.out.println(Solution5.threeSum2(b));
+        System.out.println(Solution5.threeSum(c));
+        System.out.println(Solution5.threeSum2(c));
+
+    }
+
+
+    @Test
+    public void threeSumClosest() {
+        int[] a = {-1, 2, 1, -4};
+        int[] b = {1, 1, -1, -1, 3};
+
+
+//        System.out.println(Solution5.threeSumClosest(a,1));
+        System.out.println(Solution5.threeSumClosest(b, -1));
+
+
+    }
 }
