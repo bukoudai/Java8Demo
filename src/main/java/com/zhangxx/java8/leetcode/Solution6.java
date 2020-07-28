976f51988b623244d99c38360e134a9ef8daaa98
bukoudai <secret0me@qq.com>
2020-07-28 16:53:05 +0800

    24. 两两交换链表中的节点

--- src/main/java/com/zhangxx/java8/leetcode/Solution6.java
+++ src/main/java/com/zhangxx/java8/leetcode/Solution6.java
@@ -157,4 +157,65 @@ public class Solution6 {
 
         return left;
     }
+
+    /**
+     * 24. 两两交换链表中的节点
+     * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+     * <p>
+     * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+     * <p>
+     *  
+     * <p>
+     * 示例:
+     * <p>
+     * 给定 1->2->3->4, 你应该返回 2->1->4->3.
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/swap-nodes-in-pairs
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode swapPairs2(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode ans = head.next == null ? head : head.next;
+        ListNode pre = new ListNode();
+
+        ListNode left = head;
+        pre.next = left;
+        ListNode right = head.next;
+        while (true) {
+            if (right != null) {
+                left.next = right.next;
+                right.next = left;
+                pre.next = right;
+            } else {
+                break;
+            }
+            pre = left;
+            left = left.next;
+            if (left == null) {
+                break;
+            }
+            right = left.next;
+        }
+
+        return ans;
+    }
+
+    public static ListNode swapPairs(ListNode head) {
+        //递归
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode right = head.next;
+        head.next = swapPairs(right.next);
+        right.next = head;
+
+        return right;
+
+    }
 }
