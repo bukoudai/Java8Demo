1ad6ad592a482e1561f3e95d1947c6df29ceda0a
bukoudai <secret0me@qq.com>
2020-07-30 15:51:35 +0800

    33. 搜索旋转排序数组 面向用例解题!!

--- src/main/java/com/zhangxx/java8/leetcode/Solution7.java
+++ src/main/java/com/zhangxx/java8/leetcode/Solution7.java
@@ -81,4 +81,100 @@ public class Solution7 {
         //余数是2属于情况3，直接返回3和2的组合
         return (int) (Math.pow(3, x) * 2);
     }
+
+    /**
+     * 33. 搜索旋转排序数组
+     * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+     * <p>
+     * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+     * <p>
+     * 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+     * <p>
+     * 你可以假设数组中不存在重复的元素。
+     * <p>
+     * 你的算法时间复杂度必须是 O(log n) 级别。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: nums = [4,5,6,7,0,1,2], target = 0
+     * 输出: 4
+     * 示例 2:
+     * <p>
+     * 输入: nums = [4,5,6,7,0,1,2], target = 3
+     * 输出: -1
+     * 输入: nums = [4,5,6,7,1,2,3], target = 5
+     * 输出: -1
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/search-in-rotated-sorted-array
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int search(int[] nums, int target) {
+        int length = nums.length;
+        if (length == 0) {
+            return -1;
+        }
+        if (length == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        int left = 0;
+        int right = length - 1;
+        int mid = left + ((right - left) >> 1);
+
+        while (left < right) {
+            if (nums[left] == target) {
+                return left;
+            }
+            if (nums[right] == target) {
+                return right;
+            }
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] > target) {
+                if (nums[0] > nums[mid]) {
+                    //mid在右边区间
+                    //target在右边区间
+                    right = mid - 1;
+                } else if (nums[0] > target) {
+                    //target在右边区间
+                    //mid在左边区间
+                    left = mid + 1;
+                } else if (nums[0] < target) {
+                    //target mid 在左边区间
+                    //mid在左边区间
+                    right = mid - 1;
+                } else {
+                    return -1;
+                }
+
+            } else if (target > nums[mid]) {
+
+                if (nums[0] > target) {
+                    //mid在右边区间
+                    //target在右边区间
+                    left = mid + 1;
+                } else if (nums[0] > nums[mid]) {
+                    //target在左边区间
+                    //mid在右边区间
+                    right = mid - 1;
+                } else if (nums[mid] > nums[0]) {
+                    //target在左边区间
+                    //mid在左边区间
+                    left = mid + 1;
+                } else {
+                    return -1;
+                }
+
+            }
+            mid = left + ((right - left) >> 1);
+        }
+        return nums[mid] != target ? -1 : mid;
+
+
+    }
 }
--- src/test/java/LeetCodeTest7.java
+++ src/test/java/LeetCodeTest7.java
@@ -1,14 +1,26 @@
-import com.zhangxx.java8.leetcode.Solution6;
+import com.zhangxx.java8.leetcode.Solution7;
 import org.junit.Test;
 
 public class LeetCodeTest7 {
 
     @Test
-    public void longestValidParentheses() {
-
-        System.out.println(Solution6.longestValidParentheses(")())"));
-        System.out.println(Solution6.longestValidParentheses(")()())"));
-        System.out.println(Solution6.longestValidParentheses("()(())"));
+    public void search() {
+        int[] a = new int[]{4, 5, 6, 7, 0, 1, 2};
+        int[] b = new int[]{3, 5, 1};
+        int[] c = new int[]{5, 1, 3};
+        int[] d = new int[]{4, 5, 6, 7, 8, 1, 2, 3};
+        int[] e = new int[]{8, 9, 2, 3, 4, 5, 6, 7};
+        int[] f = new int[]{5, 1, 2, 3, 4};
+        int[] g = new int[]{1, 3};
+        System.out.println(Solution7.search(a, 0));
+        System.out.println(Solution7.search(a, 3));
+        System.out.println(Solution7.search(b, 3));
+        System.out.println(Solution7.search(b, 1));
+        System.out.println(Solution7.search(c, 3));
+        System.out.println(Solution7.search(d, 8));
+        System.out.println(Solution7.search(e, 6));
+        System.out.println(Solution7.search(f, 1));
+        System.out.println(Solution7.search(g, 2));
 
 
     }
