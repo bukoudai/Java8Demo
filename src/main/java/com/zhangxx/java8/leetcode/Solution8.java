3337060f2c738a04a824a6018a4065d6b01fd92a
bukoudai <secret0me@qq.com>
2020-07-31 15:42:38 +0800

    206. 反转链表 剑指 Offer 24. 反转链表

--- src/main/java/com/zhangxx/java8/leetcode/Solution8.java
+++ src/main/java/com/zhangxx/java8/leetcode/Solution8.java
@@ -53,4 +53,76 @@ public class Solution8 {
         return move;
 
     }
+
+    /**
+     * 206. 反转链表
+     * 剑指 Offer 24. 反转链表
+     * 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+     * <p>
+     *  
+     * <p>
+     * 示例:
+     * <p>
+     * 输入: 1->2->3->4->5->NULL
+     * 输出: 5->4->3->2->1->NULL
+     *  
+     * <p>
+     * 限制：
+     * <p>
+     * 0 <= 节点个数 <= 5000
+     * `
+     * 1-2-3-4-5-null
+     * <p>
+     * 1-null
+     * <p>
+     * 2-1
+     * 3-2
+     * 4-3
+     * 5-4
+     * <p>
+     * null
+     * <p>
+     * <p>
+     * 注意：本题与主站 206 题相同：https://leetcode-cn.com/problems/reverse-linked-list/
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public static ListNode reverseList(ListNode head) {
+        //递归 脑子不够用了..
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode p = reverseList(head.next);
+        ListNode tmp = head.next;
+        head.next = null;
+        tmp.next = head;
+        return p;
+    }
+
+    //     * 不知道自己怎么做出了的!!
+    //     * 就那样成功了 这就是简单题么?
+    public static ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode pre = new ListNode();
+        pre.next = head;
+        ListNode two = head.next;
+        ListNode tmp;
+        head.next = null;
+        while (true) {
+            tmp = two.next;
+            two.next = pre.next;
+            pre.next = two;
+            if (tmp != null) {
+                two = tmp;
+            } else {
+                break;
+            }
+        }
+
+        return pre.next;
+    }
 }
--- src/test/java/LeetCodeTest8.java
+++ src/test/java/LeetCodeTest8.java
@@ -1,3 +1,4 @@
+import com.zhangxx.java8.leetcode.ListNode;
 import com.zhangxx.java8.leetcode.Solution8;
 import org.junit.Test;
 
@@ -9,6 +10,20 @@ public class LeetCodeTest8 {
 
 //        System.out.println( Solution7.firstMissingPositive(a));
         System.out.println(Solution8.minMoves2(a));
+
+
+    }
+
+    @Test
+    public void reverseList() {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println(Solution8.reverseList(head));
 
 
     }
