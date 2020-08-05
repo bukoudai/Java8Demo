8b7bde8fd1210f7bbaa19f444b99630cd63e69a1
bukoudai <secret0me@qq.com>
2020-08-05 14:11:39 +0800

    198. 打家劫舍

---
+++ src/test/java/LeetCodeTest9.java
@@ -0,0 +1,15 @@
+import com.zhangxx.java8.leetcode.Solution9;
+import org.junit.Test;
+
+public class LeetCodeTest9 {
+
+
+    @Test
+    public void maxSubArray() {
+
+
+        System.out.println(Solution9.rob(new int[]{2, 7, 9, 11, 1}));
+
+
+    }
+}
