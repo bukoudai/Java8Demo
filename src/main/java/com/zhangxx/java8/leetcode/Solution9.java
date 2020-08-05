5499c0663b5f97cdd76db1ff4c5168e8de4a9543
bukoudai <secret0me@qq.com>
2020-08-05 15:16:14 +0800

    213. 打家劫舍 II

--- src/main/java/com/zhangxx/java8/leetcode/Solution9.java
+++ src/main/java/com/zhangxx/java8/leetcode/Solution9.java
@@ -39,11 +39,9 @@ public class Solution9 {
     public static int rob(int[] nums) {
         if (nums.length == 0) {
             return 0;
-
         }
         if (nums.length == 1) {
             return nums[0];
-
         }
         int ans = nums[0];
         int[] dp = new int[nums.length + 1];
@@ -56,4 +54,64 @@ public class Solution9 {
         }
         return ans;
     }
+
+    /**
+     * 213. 打家劫舍 II
+     * 你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都围成一圈，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+     * <p>
+     * 给定一个代表每个房屋存放金额的非负整数数组，计算你在不触动警报装置的情况下，能够偷窃到的最高金额。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: [2,3,2]
+     * 输出: 3
+     * 解释: 你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
+     * 示例 2:
+     * <p>
+     * 输入: [1,2,3,1]
+     * 输出: 4
+     * 解释: 你可以先偷窃 1 号房屋（金额 = 1），然后偷窃 3 号房屋（金额 = 3）。
+     *      偷窃到的最高金额 = 1 + 3 = 4 。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/house-robber-ii
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @return
+     */
+    public static int rob_2(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        //不算最后一个
+        int ans1 = nums[0];
+        int[] dp1 = new int[nums.length + 1];
+        dp1[1] = nums[0];
+        dp1[2] = Math.max(nums[0], nums[1]);
+        ans1 = Math.max(ans1, dp1[2]);
+        for (int i = 3; i < dp1.length - 1; i++) {
+            dp1[i] = Math.max(nums[i - 1] + dp1[i - 2], nums[i - 1] + dp1[i - 3]);
+            ans1 = Math.max(ans1, dp1[i]);
+        }
+
+        //不算第一个
+        int ans2 = nums[0];
+        int[] dp2 = new int[nums.length + 1];
+        dp2[1] = 0;
+        dp2[2] = nums[1];
+        ans2 = Math.max(ans2, dp2[2]);
+        for (int i = 3; i < dp2.length; i++) {
+            dp2[i] = Math.max(nums[i - 1] + dp2[i - 2], nums[i - 1] + dp2[i - 3]);
+            ans2 = Math.max(ans2, dp2[i]);
+        }
+
+        //比较两次大小
+        return Math.max(ans2, ans1);
+    }
+
+
 }
--- src/test/java/LeetCodeTest9.java
+++ src/test/java/LeetCodeTest9.java
@@ -5,10 +5,23 @@ public class LeetCodeTest9 {
 
 
     @Test
-    public void maxSubArray() {
+    public void rob() {
 
 
         System.out.println(Solution9.rob(new int[]{2, 7, 9, 11, 1}));
+
+
+    }
+
+    @Test
+    public void rob_2() {
+
+
+        System.out.println(Solution9.rob_2(new int[]{2, 7, 9, 11, 1}));
+        System.out.println(Solution9.rob_2(new int[]{2, 3, 2}));
+        System.out.println(Solution9.rob_2(new int[]{1, 1, 1, 2}));
+        System.out.println(Solution9.rob_2(new int[]{2, 2, 4, 3, 2, 5}));
+        System.out.println(Solution9.rob_2(new int[]{2, 1, 2, 6, 1, 8, 10, 10}));
 
 
     }
