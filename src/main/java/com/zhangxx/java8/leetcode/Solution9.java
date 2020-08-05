1d73b63236eb72da6427fdbae1e14e33d71974ab
bukoudai <secret0me@qq.com>
2020-08-05 16:58:20 +0800

    337. 打家劫舍 III TreeNode 树形输出

--- src/main/java/com/zhangxx/java8/leetcode/Solution9.java
+++ src/main/java/com/zhangxx/java8/leetcode/Solution9.java
@@ -1,10 +1,14 @@
 package com.zhangxx.java8.leetcode;
 
+import java.util.HashMap;
+
 public class Solution9 {
 
     /**
      * 198. 打家劫舍
-     * 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+     * 你是一个专业的小偷，计划偷窃沿街的房屋。
+     * 每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，
+     * 如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
      * <p>
      * 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
      * <p>
@@ -57,7 +61,9 @@ public class Solution9 {
 
     /**
      * 213. 打家劫舍 II
-     * 你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都围成一圈，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+     * 你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。
+     * 这个地方所有的房屋都围成一圈，这意味着第一个房屋和最后一个房屋是紧挨着的。
+     * 同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
      * <p>
      * 给定一个代表每个房屋存放金额的非负整数数组，计算你在不触动警报装置的情况下，能够偷窃到的最高金额。
      * <p>
@@ -113,5 +119,120 @@ public class Solution9 {
         return Math.max(ans2, ans1);
     }
 
+    /**
+     * 337. 打家劫舍 III
+     * <p>
+     * 真是道高一尺魔高一丈啊...
+     * 在上次打劫完一条街道之后和一圈房屋后，小偷又发现了一个新的可行窃的地区。
+     * 这个地区只有一个入口，我们称之为“根”。 除了“根”之外，每栋房子有且只有一个“父“房子与之相连。
+     * 一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。
+     * 如果两个直接相连的房子在同一天晚上被打劫，房屋将自动报警。
+     * <p>
+     * 计算在不触动警报的情况下，小偷一晚能够盗取的最高金额。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: [3,2,3,null,3,null,1]
+     * <p>
+     * 3
+     * / \
+     * 2   3
+     * \   \
+     * 3  3
+     * \   \
+     * 3   1
+     * <p>
+     * 输出: 7
+     * 解释: 小偷一晚能够盗取的最高金额 = 3 + 3 + 1 = 7.
+     * 示例 2:
+     * <p>
+     * 输入: [3,4,5,1,3,null,1]
+     * <p>
+     *      3
+     * / \
+     * 4    5
+     * / \   \
+     * 1   3   1
+     * <p>
+     * 输出: 9
+     * 解释: 小偷一晚能够盗取的最高金额 = 4 + 5 = 9.
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/house-robber-iii
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param root
+     * @return
+     */
+    private static HashMap<TreeNode, Integer> rob = new HashMap<>();
+
+    private static int rob(TreeNode root) {
+        if (rob.get(root) != null) {
+            return rob.get(root);
+        }
+        if (root == null) {
+            rob.put(root, 0);
+            return 0;
+        }
+        rob.put(root, Math.max(rob_no_select(root), rob_select(root)));
+        return rob.get(root);
+    }
+
+    /**
+     * 不选择根节点 的最大值
+     *
+     * @param root
+     * @return
+     */
+    private static int rob_no_select(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int ans = 0;
+        if (root.right != null) {
+            ans = ans + rob(root.right);
+        }
+        if (root.left != null) {
+            ans = ans + rob(root.left);
+        }
+        return ans;
+    }
+
+    /**
+     * 选择根节点的最大值
+     *
+     * @param root
+     * @return
+     */
+    private static int rob_select(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        return root.val + rob_no_select(root.left) + rob_no_select(root.right);
+    }
+
+
+    public static int rob2(TreeNode root) {
+        int[] result = robInternal(root);
+        return Math.max(result[0], result[1]);
+    }
+
+    // 返回数组 0 保存不选根节点 1 保存选择根节点
+    private static int[] robInternal(TreeNode root) {
+        if (root == null) {
+            return new int[2];
+        }
+        int[] result = new int[2];
+
+        int[] left = robInternal(root.left);
+        int[] right = robInternal(root.right);
+
+        result[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        result[1] = left[0] + right[0] + root.val;
+
+        return result;
+    }
 
 }
--- src/test/java/LeetCodeTest9.java
+++ src/test/java/LeetCodeTest9.java
@@ -1,4 +1,6 @@
+import com.zhangxx.java8.leetcode.Solution4;
 import com.zhangxx.java8.leetcode.Solution9;
+import com.zhangxx.java8.leetcode.TreeNode;
 import org.junit.Test;
 
 public class LeetCodeTest9 {
@@ -22,6 +24,17 @@ public class LeetCodeTest9 {
         System.out.println(Solution9.rob_2(new int[]{1, 1, 1, 2}));
         System.out.println(Solution9.rob_2(new int[]{2, 2, 4, 3, 2, 5}));
         System.out.println(Solution9.rob_2(new int[]{2, 1, 2, 6, 1, 8, 10, 10}));
+
+
+    }
+
+    @Test
+    public void rob_3() {
+        int[] a = {10, 3, 0, 5, 9};
+
+        TreeNode treeNode = Solution4.sortedArrayToBST(a);
+
+        System.out.println(Solution9.rob2(treeNode));
 
 
     }
