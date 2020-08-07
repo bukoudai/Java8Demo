c0f8a242db33ebb64ea33005a3385ca416770813
bukoudai <secret0me@qq.com>
2020-08-07 15:35:04 +0800

    61. 旋转链表

--- src/main/java/com/zhangxx/java8/leetcode/Solution9.java
+++ src/main/java/com/zhangxx/java8/leetcode/Solution9.java
@@ -413,5 +413,54 @@ public class Solution9 {
 
     }
 
+    /**
+     * 61. 旋转链表
+     * 给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: 1->2->3->4->5->NULL k=2
+     * 输出: 4->5->1->2->3->NULL
+     * 解释:
+     * 向右旋转 1 步: 5->1->2->3->4->NULL
+     * 向右旋转 2 步: 4->5->1->2->3->NULL
+     * 示例 2:
+     * <p>
+     * 输入: 0->1->2->NULL, k = 4 s=3
+     * 输出: 2->0->1->NULL
+     * 解释:
+     * 向右旋转 1 步: 2->0->1->NULL
+     * 向右旋转 2 步: 1->2->0->NULL
+     * 向右旋转 3 步: 0->1->2->NULL
+     * 向右旋转 4 步: 2->0->1->NULL
+     * <p>
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/rotate-list
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null) {
+            return head;
+        }
 
+        ArrayList<ListNode> a = new ArrayList<>();
+        //把链表转换成列表
+        while (true) {
+            a.add(head);
+            if (head.next == null) {
+                //首尾相连
+                head.next = a.get(0);
+                break;
+            }
+            head = head.next;
+        }
+        int size = a.size();
+        int i = (k) % (size);
+        return i == 0 ? a.get(0) : a.get(size - i);
+    }
 }
