92e634200dd21c322e114ef613873f4160a155eb
bukoudai <secret0me@qq.com>
2020-08-14 16:40:42 +0800

    55. 跳跃游戏

--- src/main/java/com/zhangxx/java8/leetcode/Solution10.java
+++ src/main/java/com/zhangxx/java8/leetcode/Solution10.java
@@ -267,4 +267,56 @@ public class Solution10 {
         return ans;
 
     }
+
+
+    /**
+     * 55. 跳跃游戏
+     * 给定一个非负整数数组，你最初位于数组的第一个位置。
+     * <p>
+     * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+     * <p>
+     * 判断你是否能够到达最后一个位置。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: [2,3,1,1,4]
+     * 输入: [2,4,4,1,4]
+     * 输出: true
+     * 解释: 我们可以先跳 1 步，从位置 0 到达 位置 1, 然后再从位置 1 跳 3 步到达最后一个位置。
+     * 示例 2:
+     * <p>
+     * 输入: [3,2,1,0,4]
+     * 输出: false
+     * 解释: 无论怎样，你总会到达索引为 3 的位置。但该位置的最大跳跃长度是 0 ， 所以你永远不可能到达最后一个位置。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/jump-game
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @return
+     */
+    public static boolean canJump(int[] nums) {
+        int length = nums.length;
+
+        if (length == 0) {
+            return false;
+        }
+        int max = 0;
+        for (int i = 0; i < length; i++) {
+            max = Math.max(nums[i] + i, max);
+            if (max >= (length - 1)) {
+                return true;
+            }
+
+            if ((i >= (max)) && (i != (length - 1))) {
+                return false;
+            }
+
+        }
+
+
+        return false;
+
+    }
 }
