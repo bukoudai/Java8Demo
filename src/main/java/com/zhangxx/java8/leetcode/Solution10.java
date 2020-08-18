2289ae66458080a88e69a0effb6fd2fc0490069e
bukoudai <secret0me@qq.com>
2020-08-18 15:27:28 +0800

    109. 有序链表转换二叉搜索树

--- src/main/java/com/zhangxx/java8/leetcode/Solution10.java
+++ src/main/java/com/zhangxx/java8/leetcode/Solution10.java
@@ -536,4 +536,63 @@ public class Solution10 {
 
 
     }
+
+    /**
+     * 109. 有序链表转换二叉搜索树
+     * 给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+     * <p>
+     * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+     * <p>
+     * 示例:
+     * <p>
+     * 给定的有序链表： [-10, -3, 0, 5, 9],
+     * <p>
+     * 一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
+     * <p>
+     * 0
+     * / \
+     * -3   9
+     * /   /
+     * -10  5
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/convert-sorted-list-to-binary-search-tree
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param head
+     * @return
+     */
+    public TreeNode sortedListToBST(ListNode head) {
+        ArrayList<Integer> n = new ArrayList<>();
+
+        if (head == null) {
+            return null;
+        }
+
+        while (head != null) {
+
+            n.add(head.val);
+            head = head.next;
+        }
+
+
+        return sortedArrayToBST(0, n.size() - 1, n);
+    }
+
+    public static TreeNode sortedArrayToBST(int start, int end, ArrayList<Integer> nums) {
+
+        if (end == start) {
+            return new TreeNode(nums.get(end));
+        } else if (end < start) {
+            return null;
+        }
+        int mid = (start + end) >>> 1;
+        TreeNode root = new TreeNode(nums.get(mid));
+        root.left = sortedArrayToBST(start, mid - 1, nums);
+        root.right = sortedArrayToBST(mid + 1, end, nums);
+
+        return root;
+    }
+
+
 }
