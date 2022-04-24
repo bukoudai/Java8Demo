6316e3df405c77436a84436a6ba741eabc58fdb2
bukoudai <secret0me@qq.com>
2022-04-24 09:17:53 +0800

    396. 旋转函数

--- src/test/java/LeetCodeTest12.java
+++ src/test/java/LeetCodeTest12.java
@@ -1,2 +1,21 @@
+import com.zhangxx.java8.leetcode.Solution12;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("LeetCodeTest12")
 public class LeetCodeTest12 {
+
+  @Disabled
+  @Test
+  public void simplifyPath() {
+
+
+//    System.out.println(Solution12.maxRotateFunction(new int[]{100}));
+//    System.out.println(Solution12.maxRotateFunction(new int[]{4,3,2,6}));
+    System.out.println(Solution12.maxRotateFunction(new int[]{4,3,2,9}));
+
+
+
+  }
 }
